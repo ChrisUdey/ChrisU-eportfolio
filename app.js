13f40f17dfbaf5d953537821bea7afcace75cfcd
Chris Udey <udey4948@saskpolytech.ca>
2025-09-06 10:16:54 -0600

    Added more to portfolio

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const projectRouter = require('./routes/projects');
+
 
 var app = express();
 
@@ -18,9 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bw', express.static(path.join(__dirname + '/node_modules/bootswatch/dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/projects', projectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',
+      {
+        title: 'Welcome to my e-portfolio',
+        aboutMe: "My name is Chris Udey I am 31 years old",
+        picture: "/images/profile.jpg"
+
+      });
 });
 
+router.get('/aboutMe', function(req, res, next) {
+    res.render('aboutMe', {
+        title: 'About Me - Chris Udey',
+    });
+});
+
+
+router.get('/projects', function(req, res) {
+    res.render('projects', {
+        title: 'Projects - Chris Udey',
+    });
+});
+
+router.get('/skills', function(req, res) {
+    res.render('skills', {
+        title: 'Skills - Chris Udey',
+    });
+});
 module.exports = router;
